6378ad129f6d4126ddbd144d545d7889ad2c7dad
Veli DAŞAN <94240815+VeliDasan@users.noreply.github.com>
2024-11-21 13:03:07 +0300

    Update video_repository.dart

--- lib/src/repositories/video_repository.dart
+++ lib/src/repositories/video_repository.dart
@@ -4,10 +4,10 @@ import '../models/CameraModel.dart';
 
 class CameraRepository {
   final List<CameraModel> cameras = [
-    CameraModel("rtsp://admin:Sd2023!*@@188.59.119.126:554/cam/realmonitor?channel=2&subtype=1"),
-    CameraModel("rtsp://admin:Sd2023!*@@188.59.119.126:554/cam/realmonitor?channel=1&subtype=1"),
-    CameraModel("rtsp://admin:Sd2023!*@@188.59.119.126:554/cam/realmonitor?channel=1&subtype=1"),
-    CameraModel("rtsp://admin:Sd2023!*@@188.59.119.126:554/cam/realmonitor?channel=1&subtype=1"),
+    CameraModel("rtsp:..."),
+    CameraModel("rtsp:..."),
+    CameraModel("rtsp:..."),
+    CameraModel("rtsp:..."),
   ];
 
   List<CameraModel> getCameras() {
